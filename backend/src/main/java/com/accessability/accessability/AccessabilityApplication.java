afd4b5982618f40376d59328e5e777645f52afd6
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-09-21 09:16:48 +0200

    initial commit

---
+++ backend/src/main/java/com/accessability/accessability/AccessabilityApplication.java
@@ -0,0 +1,13 @@
+package com.accessability.accessability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessabilityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/accessability/accessability/AccessabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accessability.accessability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
