a0511c5975e3eba5945df83148d05a520599320d
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-09-25 10:23:24 +0200

    tested getters on front

---
+++ frontend/src/views/LandingView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ApiConnection from '../services/ApiConnection'
+import { onMounted, ref } from 'vue'
+
+const stores = ref([])
+const characteristics = ref([])
+
+const getStores = async () => {
+    let response = await ApiConnection.getAllStores()
+    stores.value = response.data
+    console.log(stores.value);
+    return stores
+}
+
+const getCharacteristics = async () => {
+    let response = await ApiConnection.getAllCharacteristics()
+    characteristics.value = response.data
+    console.log(characteristics.value);
+    return characteristics
+}
+
+onMounted(() => {
+    getStores()
+    getCharacteristics()
+})
+</script>
+
+<template>
+    <div>
+        Hola
+    </div>
+</template>
