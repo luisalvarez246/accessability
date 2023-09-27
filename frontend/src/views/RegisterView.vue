d027ece5db83605a956bdb904aaeba5172bd319b
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-09-27 14:24:15 +0200

    added icons for every characteristic

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -70,7 +70,7 @@ const updateCheckbox = (id, value) => {
 
 const submit = handleSubmit((values) => {
   alert(JSON.stringify(values, null, 2));
-  
+
 });
 
 const getAllCharacteristics = async () => {
@@ -91,51 +91,25 @@ onBeforeMount(() => {
   </div>
 
   <form @submit.prevent="submit">
-    <v-text-field
-      v-model="name.value.value"
-      :counter="10"
-      :error-messages="name.errorMessage.value"
-      label="Name"
-    ></v-text-field>
-    
+    <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+      label="Name"></v-text-field>
 
 
-    <v-text-field
-      v-model="city.value.value"
-      :counter="7"
-      :error-messages="city.errorMessage.value"
-      label="City"
-      placeholder="Gijón, Oviedo, Avilés..."
-    ></v-text-field>
 
-    <v-text-field
-      v-model="phone.value.value"
-      :counter="7"
-      :error-messages="phone.errorMessage.value"
-      label="Phone"
-      placeholder="667123456..."
-    ></v-text-field>
+    <v-text-field v-model="city.value.value" :counter="7" :error-messages="city.errorMessage.value" label="City"
+      placeholder="Gijón, Oviedo, Avilés..."></v-text-field>
 
-    <v-text-field
-      v-model="address.value.value"
-      :error-messages="address.errorMessage.value"
-      label="Address"
-      placeholder="Calle Principal..."
-    ></v-text-field>
+    <v-text-field v-model="phone.value.value" :counter="7" :error-messages="phone.errorMessage.value" label="Phone"
+      placeholder="667123456..."></v-text-field>
 
-    <v-text-field
-      v-model="type.value.value"
-      :error-messages="type.errorMessage.value"
-      label="Type"
-      placeholder="Restaurant, Hotel..."
-    ></v-text-field>
+    <v-text-field v-model="address.value.value" :error-messages="address.errorMessage.value" label="Address"
+      placeholder="Calle Principal..."></v-text-field>
 
-    <v-text-field
-      v-model="email.value.value"
-      :error-messages="email.errorMessage.value"
-      label="Email"
-      placeholder="name@domain.com"
-    ></v-text-field>
+    <v-text-field v-model="type.value.value" :error-messages="type.errorMessage.value" label="Type"
+      placeholder="Restaurant, Hotel..."></v-text-field>
+
+    <v-text-field v-model="email.value.value" :error-messages="email.errorMessage.value" label="Email"
+      placeholder="name@domain.com"></v-text-field>
 
     <!-- <v-checkbox
     class="d-flex "
@@ -147,7 +121,7 @@ onBeforeMount(() => {
       type="checkbox"
     ></v-checkbox> -->
 
-    <v-checkbox
+    <!-- <v-checkbox
       v-for="characteristic in characteristics"
       :key="characteristic.id"
       :model-value="checkboxValues[characteristic.id]"
@@ -155,14 +129,25 @@ onBeforeMount(() => {
       :error-messages="checkbox.errorMessage.value"
       :value="characteristic.id"
       :label="characteristic.title"
-      :img="characteristic.image"
       type="checkbox"
     ></v-checkbox>
+    <v-img :src="characteristic.icon"></v-img> -->
 
-    <v-textarea
-     label="Description"
-     placeholder="Enter a detailed description of the characteristics..."
-     ></v-textarea>
+    <div v-for="characteristic in characteristics" :key="characteristic.id">
+      <v-img
+
+        class="w-25"
+       :src="characteristic.icon"
+       ></v-img>
+      <v-checkbox
+        :model-value="checkboxValues[characteristic.id]" @update:model-value="updateCheckbox(characteristic.id, $event)"
+        :error-messages="checkbox.errorMessage.value" :value="characteristic.id" :label="characteristic.title"
+        type="checkbox"></v-checkbox>
+
+    </div>
+
+
+    <v-textarea label="Description" placeholder="Enter a detailed description of the characteristics..."></v-textarea>
 
     <v-btn rounded-sm class="me-4 bg-green-lighten-1" type="submit"> submit </v-btn>
     <!-- <v-btn rounded="me-4 green-lighten-1" type="submit">Rounded xs</v-btn> -->
