033264e1edd7e1dbb0786bd4fffd4f6566c2855c
Miguel <miguelpechero80@gmail.com>
2023-09-27 16:07:27 +0200

    changed to 2 cards

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -6,8 +6,8 @@
 <template>
   <v-container grid-list-md>
     <v-row>
-      <v-flex cols="12" sm="6" md="6" lg="4" class="mx-auto">
-        <v-card color="error" class="mr-12 mb-7" max-width="400" elevation="10">
+      <v-flex cols="12" sm="6" md="6" lg="6" class="mx-auto">
+        <v-card color="#59029F" class="mr-12 mb-7" min-width="600" max-width="650" elevation="10">
           <v-img
             src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-restaurantes-que-tienes-que-probar-en-madrid-1644914076.jpeg?crop=0.753xw:1.00xh;0.125xw,0&resize=1200:*"
           ></v-img>
@@ -15,24 +15,22 @@
             <h2>Restaurante Casa Pepe</h2>
           </v-card-title>
           <v-card-text>
-            <p>La mejor casa de comidas de todo Gijón</p>
+            <p>La mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo Gijón</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn class="text-none text-subtitle-1 mx-auto" variant="flat" color="#5865f2" block>MORE INFO</v-btn>
+            <v-btn class="text-none text-subtitle-1 mx-auto" variant="flat" color="#7ED057" block>MORE INFO</v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
 
-      <v-flex cols="12" sm="6" md="6" lg="4" class="mx-auto">
-        <v-card class="mr-12 mb-7" max-width="400" elevation="10">
-          <v-img
-            src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-restaurantes-que-tienes-que-probar-en-madrid-1644914076.jpeg?crop=0.753xw:1.00xh;0.125xw,0&resize=1200:*"
-          ></v-img>
+      <v-flex cols="12" sm="6" md="6" lg="6" class="mx-auto">
+        <v-card class="mr-12 mb-7" min-width="600" max-width="650" elevation="10">
+          <v-img src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-restaurantes-que-tienes-que-probar-en-madrid-1644914076.jpeg?crop=0.753xw:1.00xh;0.125xw,0&resize=1200:*"></v-img>
           <v-card-title>
             <h2>Restaurante Casa Pepe</h2>
           </v-card-title>
           <v-card-text>
-            <p>La mejor casa de comidas de todo Oviedo</p>
+            <p>La mejor casa de comidas de todo OviedoLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo GijónLa mejor casa de comidas de todo Gijón</p>
           </v-card-text>
           <v-card-actions>
             <v-btn class="text-none text-subtitle-1 mx-auto" color="#5865f2" size="large" variant="elevated"> MORE INFO</v-btn>
@@ -40,39 +38,7 @@
         </v-card>
       </v-flex>
 
-      <v-flex cols="12" sm="6" md="6" lg="4" class="mx-auto">
-        <v-card class="mr-12 mb-7" max-width="400" elevation="10">
-          <v-img
-            src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-restaurantes-que-tienes-que-probar-en-madrid-1644914076.jpeg?crop=0.753xw:1.00xh;0.125xw,0&resize=1200:*"
-          ></v-img>
-          <v-card-title>
-            <h2>Restaurante Casa Pepe</h2>
-          </v-card-title>
-          <v-card-text>
-            <p>La mejor casa de comidas de todo Oviedo</p>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn background-color="yellow" color="blue">MORE INFO</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-
-      <v-flex cols="12" sm="6" md="6" lg="4" class="mx-auto">
-        <v-card class="mr-12 mb-7" max-width="400" elevation="10">
-          <v-img
-            src="https://hips.hearstapps.com/hmg-prod/images/los-mejores-restaurantes-que-tienes-que-probar-en-madrid-1644914076.jpeg?crop=0.753xw:1.00xh;0.125xw,0&resize=1200:*"
-          ></v-img>
-          <v-card-title>
-            <h2>Restaurante Casa Pepe</h2>
-          </v-card-title>
-          <v-card-text>
-            <p>La mejor casa de comidas de todo Oviedo</p>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn background-color="yellow" color="blue">MORE INFO</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
+     
     </v-row>
   </v-container>
 </template>
