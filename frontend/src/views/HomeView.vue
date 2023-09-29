7456b69ad55462022d96a244a920582b4e99aff7
cesarconte <yugurta@gmail.com>
2023-09-29 08:55:54 +0200

    changed icons size

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,9 +4,9 @@
     width="90%">
     <v-card
       color="cardbackground"
-      class="mx-auto rounded-lg py-10 mt-10"
+      class="mx-auto rounded-lg pt-5 pb-10 mt-10"
     >
-      <v-card-title class="text-center">
+      <v-card-title class="text-center pb-5">
         Discover the best accessible places in Asturias
       </v-card-title>
       <v-card class="mx-auto rounded-lg" color="mainbackground" width="90%" >
@@ -18,10 +18,18 @@
                   <span class="font-weight-bold">Location</span>
                 </v-col>
                 <v-col cols="auto">
-                  <v-icon size="xx-large" aria-hidden="true" aria-label="Search Location">mdi-map-marker</v-icon>
+                  <v-icon 
+                    class="mb-2" size="44" aria-hidden="true" aria-label="Search Location">mdi-map-marker</v-icon>
                 </v-col>
                 <v-col cols="8">
-                  <v-text-field bg-color="input" color="text" variant="outlined" dense rounded="lg" label="Search location" role="textbox"/>
+                  <v-text-field
+                  bg-color="input"
+                  color='text'
+                  variant="outlined"
+                  dense
+                  rounded="lg"
+                  label="Search location"
+                  role="textbox"/>
                 </v-col>
               </v-row>
               <v-row align="center">
@@ -30,17 +38,23 @@
                 </v-col>
                 <v-col class="pl-1" cols="auto">
                   <v-icon
-                  size="xx-large" aria-hidden="true" aria-label="Search accessibility">mdi-magnify</v-icon>
+                    class="mb-" size="44" aria-hidden="true" aria-label="Search accessibility">mdi-magnify</v-icon>
                 </v-col>
                 <v-col cols="8">
-                  <v-text-field bg-color="input" variant="outlined" dense rounded="lg" label="Search type of site" role="textbox"/>
+                  <v-text-field 
+                    bg-color="input"
+                    variant="outlined"
+                    dense
+                    rounded="lg"
+                    label="Search type of site"
+                    role="textbox"/>
                 </v-col>
               </v-row>
             </v-col>
             <v-col class="d-flex align-center justify-center" cols="3">
               <v-btn 
                 text
-                class="text-none px-12 mt-0"
+                class="text-none px-12"
                 color="searchbtn"
                 rounded="lg"
                 size="x-large"
@@ -50,63 +64,66 @@
             </v-col>
           </v-row>
         </v-card-text>
-        <v-card-text>
+        <v-card-item>
           <v-row>
             <v-col cols="4" class="d-flex justify-center pa-0">
               <v-icon 
-              size="xx-large"
-              class="mt-2"
+                size="48"
+                class="mt-2"
               >
-              mdi-heart
+                mdi-heart
               </v-icon>
             </v-col>
             <v-col cols="4" class="d-flex justify-center pa-0">
               <v-icon 
-              size="xx-large"
-              class="mt-2"
+                size="48"
+                class="mt-2"
               >
-              mdi-heart
+                mdi-heart
               </v-icon>
             </v-col>
             <v-col cols="4" class="d-flex justify-center pa-0">
               <v-icon 
-              size="xx-large"
-              class="mt-2"
+                size="48"
+                class="mt-2"
               >
-              mdi-heart
+                mdi-heart
               </v-icon>
             </v-col>
           </v-row>
           <v-row>
-              <v-col cols="4" class="pa-0">
-                <v-checkbox
-                    label="PRM (People with reduced mobility)"
-                    color="green"
-                    value="green"
-                    hide-details
-                    aria-checked="false"
-                  ></v-checkbox>
-              </v-col>
-              <v-col cols="4" class="pa-0">
-                <v-checkbox
-                  label="Visually impaired"
-                  color="green"
-                  value="green"
-                  hide-details
-                  aria-checked="false"
-                ></v-checkbox>
-              </v-col>
-              <v-col cols="4" class="pa-0">
-                <v-checkbox
-                  label="Impaired hearing"
-                  color="green"
-                  value="green"
-                  hide-details
-                  aria-checked="false"
-                ></v-checkbox>
-              </v-col>
+            <v-col cols="4" class="pt-0">
+              <v-checkbox
+                label="PRM (People with reduced mobility)"
+                color="green"
+                value="green"
+                hide-details
+                aria-checked="false"
+                class="d-flex justify-center align-center"
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="4" class="pt-0">
+              <v-checkbox
+                label="Visually impaired"
+                color="green"
+                value="green"
+                hide-details
+                aria-checked="false"
+                class="d-flex justify-center"
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="4" class="pt-0">
+              <v-checkbox
+                label="Impaired hearing"
+                color="green"
+                value="green"
+                hide-details
+                aria-checked="false"
+                class="d-flex justify-center align-center"
+              ></v-checkbox>
+            </v-col>
           </v-row>
-        </v-card-text>
+        </v-card-item>
       </v-card>
     </v-card>
   </v-container>
