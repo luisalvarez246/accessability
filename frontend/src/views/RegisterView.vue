0563cc3940b3f82a97f68636d7b8ec8a366cc9d5
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-10-02 15:24:13 +0200

    fixed desktop size checkboxes

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -4,7 +4,7 @@ import { ref, onBeforeMount } from "vue";
 import ApiConnection from "@/services/ApiConnection";
 import { useField, useForm } from "vee-validate";
 
-const { handleSubmit ,handleReset } = useForm({
+const { handleSubmit, handleReset } = useForm({
   validationSchema: {
     name(value) {
       if (value?.length >= 2) return true;
@@ -71,14 +71,14 @@ const store = ref({
 });
 
 const items = ref([
-"restaurant",
-    "museum",
-    "hotel",
-    "shops",
-    "clinic",
-    "cinema",
-    "hairdresser"
-])
+  "restaurant",
+  "museum",
+  "hotel",
+  "shops",
+  "clinic",
+  "cinema",
+  "hairdresser",
+]);
 
 const checkboxValues = ref([]);
 
@@ -92,7 +92,7 @@ const addStore = async () => {
     ...store.value,
     characteristicIds: checkboxValues.value,
     image: store.value.image[0].name,
-    type: items.value.id
+    type: items.value.id,
   };
   try {
     let response = await ApiConnection.saveStore(newStore);
@@ -113,7 +113,6 @@ const getAllCharacteristics = async () => {
 onBeforeMount(() => {
   getAllCharacteristics();
 });
-
 </script>
 
 <template>
@@ -162,7 +161,6 @@ onBeforeMount(() => {
           :error-messages="type.errorMessage.value"
           label="Type of bussiness"
         ></v-text-field> -->
-        
 
         <v-text-field
           class="w-75 v-label"
@@ -179,7 +177,6 @@ onBeforeMount(() => {
         ></v-text-field>
 
         <v-select
-        
           class="w-75 v-label"
           label="Type of businnes"
           v-model="items.id"
@@ -191,58 +188,49 @@ onBeforeMount(() => {
         <v-file-input
           class="w-50 v-label"
           v-model="store.image"
-          label="Upload your image"
+          label="Image"
           variant="filled"
           prepend-icon="mdi-camera"
         ></v-file-input>
       </div>
 
-      <v-container
-        class="bg-white w-75 mt-10 rounded mb-10 pt-10 pb-10 pl-10 pr-10 "
-      >
-        
-          <v-col
-            class=""
-            v-for="(characteristic, index) in characteristics"
-            :key="characteristic.id"
-          >
-            <v-row v-if="index % 4 === 0">
-              
-              <v-col
-                class="col-lg-4 col-md-6 col-sm-12 pr-10"
-                v-for="i in 4"
-                :key="index + i"
-              >
-              
-                <template v-if="index + i - 1 < characteristics.length">
-                    <v-img
+      
+      <v-container class="bg-white w-75 mt-10 rounded mb-10 py-10 pl-15">
+        <v-col
+          class=""
+          v-for="(characteristic, index) in characteristics"
+          :key="characteristic.id"
+        >
+          <v-row v-if="index % 5 === 0">
+            <v-col class="pr-10" v-for="i in 5" :key="index + i">
+              <template v-if="index + i - 1 < characteristics.length">
+                <v-checkbox
+                  class="checkboxes"
+                  v-model="store.characteristics"
+                  :model-value="
+                    checkboxValues[characteristics[index + i - 1].id]
+                  "
+                  @update:model-value="
+                    updateCheckbox(characteristics[index + i - 1].id, $event)
+                  "
+                  :error-messages="checkbox.errorMessage.value"
+                  :value="characteristics[index + i - 1].id"
+                  :label="characteristics[index + i - 1].title"
+                  type="checkbox"
+                  ><v-img
                     class="characteristicsIcon"
                     :src="characteristics[index + i - 1].icon"
                     :aria-label="characteristics[index + i - 1].icon"
-                  ></v-img>
-
-                  <v-checkbox
-                    v-model="store.characteristics"
-                    :model-value="
-                      checkboxValues[characteristics[index + i - 1].id]
-                    "
-                    @update:model-value="
-                      updateCheckbox(characteristics[index + i - 1].id, $event)
-                    "
-                    :error-messages="checkbox.errorMessage.value"
-                    :value="characteristics[index + i - 1].id"
-                    :label="characteristics[index + i - 1].title"
-                    type="checkbox"
-                  ></v-checkbox>
-                </template>
-              </v-col>
-            </v-row>
-          </v-col>
-        
+                  ></v-img
+                ></v-checkbox>
+              </template>
+            </v-col>
+          </v-row>
+        </v-col>
       </v-container>
       <div class="areaBtns bg-white w-75 rounded ml-auto mr-auto mb-10">
         <v-textarea
-          label="Description of your accesible characteristics"
+          label="Characteristics description"
           v-model="store.description"
           class="w-75 ml-auto mr-auto pt-10 v-label"
         ></v-textarea>
@@ -276,7 +264,10 @@ onBeforeMount(() => {
 .v-label {
   opacity: 1;
   font-weight: 900;
-  
 }
 
+.checkboxes {
+  font-weight: 900 !important;
+  color: black !important;
+}
 </style>
