85cb26ef9dc956bd6573bb747c58895b1a507f14
cesarconte <yugurta@gmail.com>
2023-10-09 16:07:03 +0200

    removed alt content from hero.jpg

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container height="250" width="100%" class="hero_section d-flex justify-center pb-0" color="mainbackground"
     rounded="lg">
-    <v-img src="../assets/images/hero.jpg" alt="girl whitout hands holding a cellphone" cover max-width="1140"
+    <v-img src="../assets/images/hero.jpg" alt="" cover max-width="1140"
       max-height="250">
       <div class="title">
         <h1 class="underline pl-4">Welcome to Accessability</h1>
