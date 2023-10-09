bfe7a43c8c20b01f48a7a7e47b1b3d5853016094
Lualvare <luisalvarez1@correo.unimet.edu.ve>
2023-10-09 11:46:21 +0200

    rename typeStore to launchStore
    launchStore loads the required data on launch (cities, types)

--- frontend/src/store/launchStore.js
+++ frontend/src/store/launchStore.js
@@ -1,21 +1,27 @@
 import { defineStore } from "pinia";
 
-export const useTypeStore = defineStore("types", {
+export const useLaunchStore = defineStore("launch", {
 	state: () =>
 	(
 		{
 			types: {},
+			cities: {},
 		}
 	),
 	getters:
 	{
 		getTypes: (state) => state.types,
+		getCities: (state) => state.cities,
 	},
 	actions:
 	{
 		setTypes(fetchedTypes)
 		{
 			this.types = fetchedTypes;
+		},
+		setCities(fetchedCities)
+		{
+			this.cities = fetchedCities;
 		}
 	}
 })
